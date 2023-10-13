fefe30c6e08f9e302c133fecdd87635a14bab5cd
cherif fourat <92321858+Cheriffouurat@users.noreply.github.com>
2023-10-13 12:22:36 +0200

    fourat cherif

--- src/main/java/tn/esprit/spring/entities/Skier.java
+++ src/main/java/tn/esprit/spring/entities/Skier.java
@@ -22,7 +22,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level=AccessLevel.PRIVATE)
 @Entity
 public class Skier implements Serializable {
-//fourat was here
+//fourat was here hhh
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	Long numSkier;
