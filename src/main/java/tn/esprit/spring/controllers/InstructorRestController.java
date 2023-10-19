0b5b88d8507fa99184b579916a4df1d1ce628ebd
Khchimi Othmen <80161433+khchimi-Othmen@users.noreply.github.com>
2023-10-19 18:31:54 +0100

    Fix SonarLint issues

--- src/main/java/tn/esprit/spring/controllers/InstructorRestController.java
+++ src/main/java/tn/esprit/spring/controllers/InstructorRestController.java
@@ -33,11 +33,12 @@ public class InstructorRestController {
         return instructorServices.retrieveAllInstructors();
     }
 
-    @Operation(description = "Update Instructor ")
+    @Operation(description = "Update Instructor")
     @PutMapping("/update")
-    public Instructor updateInstructor(@RequestBody Instructor Instructor){
-        return  instructorServices.updateInstructor(Instructor);
+    public Instructor updateInstructor(@RequestBody Instructor instructor) {
+        return instructorServices.updateInstructor(instructor);
     }
+
 
     @Operation(description = "Retrieve Instructor by Id")
     @GetMapping("/get/{id-instructor}")
